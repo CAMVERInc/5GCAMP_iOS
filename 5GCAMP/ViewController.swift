d421c67d5d2c2cc40b5989d31fe43647516c622c
comdori-wj <02145s1@gmail.com>
2024-11-04 15:02:54 +0900

    Init: 프로젝트 초기 셋팅

---
+++ 5GCAMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5GCAMP
+//
+//  Created by WONJI HA on 11/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
